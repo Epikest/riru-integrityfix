709d7044e810725a195bc89b956e6eb3029194d9
Nguyen Hoang The Vi <84650617+HuskyDG@users.noreply.github.com>
2022-08-03 12:38:26 +0700

    Ensure the result of building

--- build.sh
+++ build.sh
@@ -6,16 +6,16 @@ src_dir="$(pwd)"
 cd "$src_dir/java_module"
 # Must always be release due to R8 requirement
 chmod 777 ./gradlew
-./gradlew assembleRelease
+./gradlew assembleRelease || exit 1
 unzip "$src_dir/java_module/app/build/outputs/apk/release/app-release.apk" classes.dex -d "$src_dir/riru/template/magisk_module"
 
 # Build Riru module
 cd "$src_dir/riru" && rm -fr out
 chmod 777 ./gradlew
 if [ "$1" == "cache" ]; then
-    ./gradlew aR --build-cache
+    ./gradlew aR --build-cache || exit 1
 else
-    ./gradlew assembleRelease
+    ./gradlew assembleRelease || exit 1
 fi
     
 ./gradlew --stop
